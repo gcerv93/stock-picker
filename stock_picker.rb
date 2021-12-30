2fd00831235d37e6748fbfe0ed852cda96371ab5
gcerv93 <gcerv93@protonmail.com>
2021-12-30 16:03:04 -0600

    add comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,5 @@
 def stock_picker(array)
+  # set these outside of loop to keep track of largest profit
   best_buy = 0
   best_sell = 0
   profit = 0
@@ -9,12 +10,16 @@ def stock_picker(array)
 
     prev = num
     curr = array [idx+1]
+    # break when at end of loop
     break if curr.nil?
     prev < curr ? curr_best_buy = prev : curr_best_buy = curr
+    # move on if the current best buy number is greater than the original best buy number
+    next if curr_best_buy >= best_buy && best_buy != 0
     array.each_with_index do |n, i|
       if n > curr_best_buy && i > idx
         curr_best_sell = n if n > curr_best_sell
         if curr_best_sell - curr_best_buy > profit
+          # assign the variables when the current profit is greater than the original profit
           profit = curr_best_sell - curr_best_buy
           best_buy = curr_best_buy
           best_sell = curr_best_sell
@@ -32,3 +37,4 @@ end
 p stock_picker([17,3,6,9,15,8,6,1,10])
 p stock_picker([92, 90, 82, 100, 71, 95, 15, 30, 11])
 p stock_picker([49, 35, 23, 30, 34, 43, 12, 38, 8])
+p stock_picker([0,3,6,9,5,8,6,1,10])
